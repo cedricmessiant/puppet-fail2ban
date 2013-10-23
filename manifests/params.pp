219a15a013922357a5b3e9b0b2d1c1d1714d741d
Cédric Messiant <cedricmessiant@ecreall.com>
2013-10-23 16:14:25 +0200

    Add wheezy support in params

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,10 @@ class fail2ban::params {
       $email     = hiera('email')
       $whitelist = hiera('whitelist')
     }
+    'wheezy': {
+      $email     = hiera('email')
+      $whitelist = hiera('whitelist')
+    }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
     }
